e69940c5a15e4568e58a1585bccc349f41c65d89
chethan21-dev <chethanchowdary21@gmail.com>
2024-09-25 23:42:25 +0530

    Add files via upload

---
+++ JavaScript Arithmetic operators/JavaScript Arithmetic operators.js
@@ -0,0 +1,146 @@
+JavaScript Arithmetic operators -  
+1. It perform arithmetic on numbers (literals or variables).
+2. Arithmetic Operations operation operates on two numbers.
+
+Example - 
+<!DOCTYPE html>
+<html>
+<body>
+<h2>JavaScript Arithmetic</h2>
+<p id="demo"></p>
+<script>
+let x = 100 + 50;
+document.getElementById("demo").innerHTML = x;
+</script>
+</body>
+</html>
+
+in another way example - 
+<!DOCTYPE html>
+<html>
+<body>
+<h2>JavaScript Arithmetic</h2>
+<p id="demo"></p>
+<script>
+let a = 100;
+let b = 50;
+let x = a + b;
+document.getElementById("demo").innerHTML = x;
+</script>
+</body>
+</html>
+
+operands - 
+The numbers (in an arithmetic operation) are called operands.
+operator - 
+The operation (to be performed between the two operands) is defined by an operator.
+Example - 
+Operand- 100 
+Operator - 	+ 	
+Operand - 50
+Here the arithmetic operators classified into - 
+
+1. + operator - //addition
+<!DOCTYPE html>
+<html>
+<body>
+<h2>The + Operator</h2>
+<p id="demo"></p>
+<script>
+let x = 1;
+let y = 1;
+let z = x + y;
+document.getElementById("demo").innerHTML = z;
+</script>
+</body>
+</html>
+
+2. - operator - //subtraction
+<!DOCTYPE html>
+<html>
+<body>
+<h2>The - Operator</h2>
+<p id="demo"></p>
+<script>
+let x = 1;
+let y = 1;
+let z = x - y;
+document.getElementById("demo").innerHTML = z;
+</script>
+</body>
+</html>
+
+3. * operator - //multiplication
+<!DOCTYPE html>
+<html>
+<body>
+<h2>The * Operator</h2>
+<p id="demo"></p>
+<script>
+let x = 1;
+let y = 1;
+let z = x * y;
+document.getElementById("demo").innerHTML = z;
+</script>
+</body>
+</html>
+
+4. / operator  - //division
+<!DOCTYPE html>
+<html>
+<body>
+<h2>The / Operator</h2>
+<p id="demo"></p>
+<script>
+let x = 1;
+let y = 1;
+let z = x / y;
+document.getElementById("demo").innerHTML = z;
+</script>
+</body>
+</html>
+
+5. % operator -  //modulus (or) remainder
+<!DOCTYPE html>
+<html>
+<body>
+<h2>The % Operator</h2>
+<p id="demo"></p>
+<script>
+let x = 1;
+let y = 1;
+let z = x % y;
+document.getElementById("demo").innerHTML = z;
+</script>
+</body>
+</html>
+
+6. ++ operator - //increment
+<!DOCTYPE html>
+<html>
+<body>
+<h2>The ++ Operator</h2>
+<p id="demo"></p>
+<script>
+let x = 1;
+x++;
+let z = x;
+document.getElementById("demo").innerHTML = z;
+</script>
+</body>
+</html>
+
+7. -- operator - //decrement
+<!DOCTYPE html>
+<html>
+<body>
+<h2>The -- Operator</h2>
+<p id="demo"></p>
+<script>
+let x = 1;
+x--;
+let z = x;
+document.getElementById("demo").innerHTML = z;
+</script>
+</body>
+</html>
